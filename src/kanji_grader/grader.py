7dcf80f792c4c6a63e2b9ff7765636d8778aa7f4
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2023-09-18 00:09:07 +0200

    formatting

--- src/kanji_grader/grader.py
+++ src/kanji_grader/grader.py
@@ -2,20 +2,20 @@ from .levels import Levels
 
 
 def grade_text_by_jlpt(levels: Levels, text: str) -> dict[str, str]:
-  """
-  Given a piece of text, returns a map from the grade to the percent
-  of characters which are contained in that level.
+    """
+    Given a piece of text, returns a map from the grade to the percent
+    of characters which are contained in that level.
 
-  Non-exhaustive example: { "N5": "100%" }
-  """
-  # The level of a single kanji can be gotten from 'levels', for example
-  #
-  # l = levels.get_jlpt_level("一")
-  raise ValueError("TODO")
+    Non-exhaustive example: { "N5": "100%" }
+    """
+    # The level of a single kanji can be gotten from 'levels', for example
+    #
+    # l = levels.get_jlpt_level("一")
+    raise ValueError("TODO")
 
 
 def grade_file_by_jlpt(levels: Levels, filepath: str) -> dict[str, str]:
-  """
-  Given a filepath, read the file, then pass the contents to grade_text_by_jlpt
-  """
-  raise ValueError("TODO")
+    """
+    Given a filepath, read the file, then pass the contents to grade_text_by_jlpt
+    """
+    raise ValueError("TODO")
--- src/kanji_grader/levels.py
+++ src/kanji_grader/levels.py
@@ -4,90 +4,94 @@ import os
 
 
 class Levels:
-  """
-  Returns levels for kanji
-
-  >>> l = Levels()
-  >>> l.get_jlpt_level("a") == None
-  True
-  >>> l.get_jlpt_level("一")
-  5
-  >>> l.get_jlpt_level("茎")
-  1
-  >>> l.get_grade_level("一")
-  1
-  >>> l.get_grade_level("悠")
-  8
-  >>> l.get_jlpt_level("abc")
-  Traceback (most recent call last):
-        ...
-  ValueError: get_jlpt_level accepts a single character, but your text was 3 characters.
-  >>> l.get_jlpt_level("")
-  Traceback (most recent call last):
-        ...
-  ValueError: get_jlpt_level accepts a single character, but you gave it nothing.
-  >>> l.get_jlpt_level(None)
-  Traceback (most recent call last):
-        ...
-  ValueError: get_jlpt_level accepts a single character, but you gave it nothing.
-  >>> l.get_grade_level("abc")
-  Traceback (most recent call last):
-        ...
-  ValueError: get_grade_level accepts a single character, but your text was 3 characters.
-  >>> l.get_grade_level("")
-  Traceback (most recent call last):
-        ...
-  ValueError: get_grade_level accepts a single character, but you gave it nothing.
-  >>> l.get_grade_level(None)
-  Traceback (most recent call last):
-        ...
-  ValueError: get_grade_level accepts a single character, but you gave it nothing.
-  """
-
-  def __init__(self):
-    script_path = os.path.dirname(os.path.abspath(__file__))
-    json_filepath = os.path.join(
-      script_path,
-      "resources",
-      "kanji-jouyou.json"
-    )
-    with open(json_filepath, 'r') as f:
-      self.data = json.load(f)
-
-  def get_jlpt_level(self, kanji: str) -> Union[int, None]:
     """
-    Given a string like "日", returns the JLPT level like "N5"
+    Returns levels for kanji
 
-    If the character is not part of any JLPT level, then None is returned
-
-    If the character is not a recognized kanji, then an exception is thrown
+    >>> l = Levels()
+    >>> l.get_jlpt_level("a") == None
+    True
+    >>> l.get_jlpt_level("一")
+    5
+    >>> l.get_jlpt_level("茎")
+    1
+    >>> l.get_grade_level("一")
+    1
+    >>> l.get_grade_level("悠")
+    8
+    >>> l.get_jlpt_level("abc")
+    Traceback (most recent call last):
+          ...
+    ValueError: get_jlpt_level accepts a single character, but your text was 3 characters.
+    >>> l.get_jlpt_level("")
+    Traceback (most recent call last):
+          ...
+    ValueError: get_jlpt_level accepts a single character, but you gave it nothing.
+    >>> l.get_jlpt_level(None)
+    Traceback (most recent call last):
+          ...
+    ValueError: get_jlpt_level accepts a single character, but you gave it nothing.
+    >>> l.get_grade_level("abc")
+    Traceback (most recent call last):
+          ...
+    ValueError: get_grade_level accepts a single character, but your text was 3 characters.
+    >>> l.get_grade_level("")
+    Traceback (most recent call last):
+          ...
+    ValueError: get_grade_level accepts a single character, but you gave it nothing.
+    >>> l.get_grade_level(None)
+    Traceback (most recent call last):
+          ...
+    ValueError: get_grade_level accepts a single character, but you gave it nothing.
     """
-    if kanji is None or len(kanji) < 1:
-      raise ValueError("get_jlpt_level accepts a single character, but you gave it nothing.")
-    elif len(kanji) > 1:
-      raise ValueError(f"get_jlpt_level accepts a single character, but your text was {len(kanji)} characters.")
 
-    entry = self.data.get(kanji, None)
+    def __init__(self):
+        script_path = os.path.dirname(os.path.abspath(__file__))
+        json_filepath = os.path.join(script_path, "resources", "kanji-jouyou.json")
+        with open(json_filepath, "r") as f:
+            self.data = json.load(f)
 
-    if entry is None:
-      raise ValueError("Not a recognized kanji")
+    def get_jlpt_level(self, kanji: str) -> Union[int, None]:
+        """
+        Given a string like "日", returns the JLPT level like "N5"
 
-    return entry['jlpt_new']
+        If the character is not part of any JLPT level, then None is returned
 
-  def get_grade_level(self, kanji: str) -> Union[int, None]:
-    """
-    Given a string like "日", returns the grade level like "G3"
+        If the character is not a recognized kanji, then an exception is thrown
+        """
+        if kanji is None or len(kanji) < 1:
+            raise ValueError(
+                "get_jlpt_level accepts a single character, but you gave it nothing."
+            )
+        elif len(kanji) > 1:
+            raise ValueError(
+                f"get_jlpt_level accepts a single character, but your text was {len(kanji)} characters."
+            )
 
-    If the character is not part of any grade level, then None is returned
-    """
-    if kanji is None or len(kanji) < 1:
-      raise ValueError("get_grade_level accepts a single character, but you gave it nothing.")
-    elif len(kanji) > 1:
-      raise ValueError(f"get_grade_level accepts a single character, but your text was {len(kanji)} characters.")
+        entry = self.data.get(kanji, None)
 
-    entry = self.data.get(kanji, None)
+        if entry is None:
+            raise ValueError("Not a recognized kanji")
 
-    if entry is None:
-      raise ValueError("Not a recognized kanji")
+        return entry["jlpt_new"]
 
-    return entry['grade']
+    def get_grade_level(self, kanji: str) -> Union[int, None]:
+        """
+        Given a string like "日", returns the grade level like "G3"
+
+        If the character is not part of any grade level, then None is returned
+        """
+        if kanji is None or len(kanji) < 1:
+            raise ValueError(
+                "get_grade_level accepts a single character, but you gave it nothing."
+            )
+        elif len(kanji) > 1:
+            raise ValueError(
+                f"get_grade_level accepts a single character, but your text was {len(kanji)} characters."
+            )
+
+        entry = self.data.get(kanji, None)
+
+        if entry is None:
+            raise ValueError("Not a recognized kanji")
+
+        return entry["grade"]
