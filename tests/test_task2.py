ca6e413a780073686d1517fcd016284df7499ff4
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2023-09-17 23:44:18 +0200

    Real values for task 2 test

--- tests/test_task2.py
+++ tests/test_task2.py
@@ -13,9 +13,9 @@ def test_rashoumon():
   )
   result = grade_file_by_jlpt(levels, rashoumon_path)
 
-  assert result["N5"] == 1.0
-  assert result["N4"] == 1.0
-  assert result["N3"] == 1.0
-  assert result["N2"] == 1.0
-  assert result["N1"] == 1.0
+  assert result["N5"] == "34%"
+  assert result["N4"] == "58%"
+  assert result["N3"] == "78%"
+  assert result["N2"] == "88%"
+  assert result["N1"] == "100%"
   assert set(result.keys()) == set(["N1", "N2", "N3", "N4", "N5"])
